825e01e7451f8f7857a37a64cd1d72c6d7d251ef
Your Name <sanketnawale@example.com>
2025-12-22 16:20:26 +0100

    Fix duplicate APP_NAME causing blank screen

--- interface_config.js
+++ interface_config.js
@@ -9,8 +9,8 @@
  */
 
 var interfaceConfig = {
-    APP_NAME: 'Meetings',
-    appName: 'Meetings',
+     //APP_NAME: 'Meetings',
+     //appName: 'Meetings',
     AUDIO_LEVEL_PRIMARY_COLOR: 'rgba(255,255,255,0.4)',
     AUDIO_LEVEL_SECONDARY_COLOR: 'rgba(255,255,255,0.2)',
 
